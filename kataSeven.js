115e73c48f283d5c83efe9ab6a6ad7c522123328
Generosa Litton <glcodeworks@gmail.com>
2024-12-05 22:41:33 -0500

    two sum kata 6

--- kataSeven.js
+++ kataSeven.js
@@ -308,8 +308,8 @@ function XO(str) {
   );
 }
 
-console.log(XO("xo")); // true
-console.log(XO("xxOo")); // true
-console.log(XO("xxxm")); // false
-console.log(XO("Oo")); // false
-console.log(XO("ooom")); // false
+// console.log(XO("xo")); // true
+// console.log(XO("xxOo")); // true
+// console.log(XO("xxxm")); // false
+// console.log(XO("Oo")); // false
+// console.log(XO("ooom")); // false
