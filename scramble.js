20cdf291a430a86bde475ed082a157eb9b4a0f88
Generosa Litton <glcodeworks@gmail.com>
2025-02-22 10:48:46 +0700

    more practice

--- scramble.js
+++ scramble.js
@@ -19,70 +19,11 @@ E:
 D: input is a string, maybe an array to store letters?
 
 A:
-Initialize a scrambledString variable, assign to empty string
-Initialize a punctuation constant
-
-Iterate over the string
-If string length is less than or equal to three, return original string
-Convert input string to an array of characters
-get a subslice of the array from index 1 to 1 before the end
-Sort the array which makes punctuation first, 
-create a clean version without the punctuation
-Iterate over the original string
-  - if it is not a punctuation, append sorted character to result
-  - if it is, append the punctuation
-Append beginning and end chars to sorted array
-Return as a string
-
+Loop through the string to separate and store alphabetic characters and punctuation.
+Sort the alphabetic characters.
+Rebuild the string by placing the first character, sorted characters, punctuation, and lastly the final character in the proper order.
 */
 
-const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-
-function scramble_words(str) {
-  let scrambledString = [str[0]];
-
-  if (str.length <= 3) return str;
-
-  let sortedCleanStringArray = cleanString(str)
-    .split("")
-    .slice(1, str.length - 2)
-    .sort();
-
-  // console.log(sortedCleanStringArray);
-
-  for (let idx = 0; idx < str.length - 1; idx++) {
-    if (ALPHABET.includes(str[idx])) {
-      scrambledString.push(sortedCleanStringArray[idx]);
-      // console.log("b", scrambledString);
-    } else {
-      scrambledString.push(str[idx], sortedCleanStringArray[idx]);
-    }
-  }
-
-  if (!ALPHABET.includes(str[str.length - 1])) {
-    scrambledString.push(str[str.length - 2], str[str.length - 1]);
-  } else {
-    scrambledString.push(str[str.length - 1]);
-  }
-  return scrambledString.join("");
-}
-
-function cleanString(str) {
-  let strArray = str.split("");
-  let finalString = "";
-
-  strArray.forEach((char) => {
-    if (ALPHABET.includes(char)) {
-      finalString += char;
-    } else {
-      char = char.replace(char, "");
-      finalString += char;
-    }
-  });
-  return finalString;
-}
-
-// console.log(cleanString("-dbac."));
 let p = console.log;
 p(scramble_words("professionals")); //'paefilnoorsss'
 p(scramble_words("i")); //'i'
